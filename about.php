aae5cbe56be0bf01cb8bbc23e4ad0830838c36dd
Prajwal Bundhade <prajwalpro231@gmail.com>
2023-02-11 18:01:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css -->
+    <link rel="stylesheet" href="about.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <title>About Page</title>
+</head>
+
+<body class="mybody">
+    <header>
+        <!-- navbar -->
+    <?php
+    include ('header.php');
+    ?>
+    </header>
+    <!-- ---------about Section --------- -->
+    <div class="aboutStart">
+      <img src="images/aboutUs.jpg" alt="" srcset="">
+    
+    </div>
+    <section class="about-section">
+        <div class="container">
+            <div class="row">                
+                <div class="content-column col-lg-6 col-md-12 col-sm-12 order-2">
+                    <div class="inner-column">
+                        <div class="sec-title">
+                            <span class="title">About Our Company</span>
+                            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                        </div>
+                        <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto amet, numquam nostrum ut eius, natus quo quas eum repellat voluptates incidunt, tempore facilis quidem officiis temporibus minima soluta nulla animi! </div>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi tempore ad quibusdam, nulla earum aliquid dicta. Distinctio fuga commodi perspiciatis, saepe ea eaque vero! Alias expedita aspernatur ipsum animi magnam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rem quisquam possimus, exercitationem veniam aliquid quia, pariatur, fugit molestias quod fuga. Quia distinctio soluta fugiat sapiente cum laboriosam ipsa illo?
+                      </div>
+                        <div class="btn-box">
+                            <a href="#" class="theme-btn btn-style-one">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Image Column -->
+                <div class="image-column col-lg-6 col-md-12 col-sm-12">
+                   <div class="image1">
+                    <img src="images/company.jpg" alt="">
+                   </div>
+                </div>
+              
+            </div>
+           
+               
+        </div>
+    </section>
+     <!-- ---------about Section end--------- -->
+
+
+     <!-- ---------Team Section --------- -->
+    <section id="team" class="team-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="site-heading text-center">
+              <h2>Our <span>Team</span></h2>
+              <h4>Meet our awesome and expert team members</h4>
+            </div>
+          </div>
+        </div>
+        <div class="row team-items">
+          <div class="col-md-3 single-item">
+           
+            <div class="item">
+              <div class="thumb">
+                <img class="img-fluid" src="images/banner.png" alt="Thumb">
+                <div class="overlay">
+                  <h4>Lorem Ipsum</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div class="social">
+                    <ul >
+                      <li class="facebook">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                      </li>
+                      <li class="linkedin">
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                      </li>
+                      <li class="instagram">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                      </li>
+                      <li class="email">
+                        <a href="#"><i class="fa-regular fa-envelope-open"></i></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="info">
+                <span class="message">
+                  <a href="#"><i class="fas fa-envelope-open"></i></a>
+                </span>
+                <h4>Lorem Ipsum</h4>
+                <span>Project Manager</span>
+              </div>
+            </div>
+           
+          </div>
+          <div class="col-md-3 single-item">
+            <div class="item">
+              <div class="thumb">
+                <img class="img-fluid" src="images/banner.png" alt="Thumb">
+                <div class="overlay">
+                  <h4>Lorem Ipsum</h4>
+                  <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry
+                  </p>
+                  <div class="social">
+                    <ul>
+                      <li class="facebook">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                      </li>
+                      <li class="linkedin">
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                      </li>
+                      <li class="instagram">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                      </li>
+                      <li class="email">
+                        <a href="#"><i class="fa-regular fa-envelope-open"></i></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="info">
+                <span class="message">
+                  <a href="#"><i class="fas fa-envelope-open"></i></a>
+                </span>
+                <h4>Lorem Ipsum</h4>
+                <span>App Developer</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 single-item">
+            <div class="item">
+              <div class="thumb">
+                <img class="img-fluid" src="images/banner.png" alt="Thumb">
+                <div class="overlay">
+                  <h4>Lorem Ipsum</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div class="social">
+                    <ul>
+                      <li class="facebook">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                      </li>
+                      <li class="linkedin">
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                      </li>
+                      <li class="instagram">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                      </li>
+                      <li class="email">
+                        <a href="#"><i class="fa-regular fa-envelope-open"></i></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="info">
+                <span class="message">
+                  <a href="#"><i class="fas fa-envelope-open"></i></a>
+                </span>
+                <h4>Lorem Ipsum</h4>
+                <span>Web designer</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 single-item">
+            <div class="item">
+              <div class="thumb">
+                <img class="img-fluid" src="images/banner.png" alt="Thumb">
+                <div class="overlay">
+                  <h4>Lorem Ipsum</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+                  <div class="social">
+                    <ul>
+                      <li class="facebook">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                      </li>
+                      <li class="linkedin">
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                      </li>
+                      <li class="instagram">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                      </li>
+                      <li class="email">
+                        <a href="#"><i class="fa-regular fa-envelope-open"></i></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="info">
+                <span class="message">
+                  <a href="#"><i class="fas fa-envelope-open"></i></a>
+                  
+                </span>
+                <h4>Lorem Ipsum</h4>
+                <span>Project Manager</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+     <!-- ---------Team Section end--------- -->
+
+
+    <!-- -------Footer----- -->
+<?php
+include ('footer.php');
+?>
+
+    <!-- --------footer end---- -->
+
+    <!-- js -->
+    <script src="https://kit.fontawesome.com/f2786c0b81.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css -->
+    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="services.css">
+    <!-- Bootstrap --> 
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <title>Service Page</title>
+</head>
+
+<body class="mybody">
+    <header>
+        <!-- navbar -->
+    <?php
+    include ('header.php');
+    ?>
+    </header>
+    <!-- --------- --------- -->
+    <div class="aboutStart">
+      <img src="images/Services.jpg" alt="" srcset="">
+    
+    </div>
+<!-- ---------Services Page------ -->
+
+<section class="we-offer-area text-center bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="site-heading text-center">
+                        <h2>Our <span>Services</span></h2>
+                        <h4>Lorem Ipsum is simply dummy text</h4>
+                        
+                    </div>
+                </div>
+            </div>
+                <div class="row our-offer-items less-carousel">
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn"><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn"><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn"><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn"><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height ">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn"><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+
+                    <!-- Single Item -->
+                    <div class="col-md-4 col-sm-6 equal-height">
+                        <div class="item">
+                            <i class="fas fa-building"></i>
+                            <h4>Building Construction</h4>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </p>
+                            <p class="more-btn" ><a href="#">Know More</a></p>
+                        </div>
+                    </div>
+                    <!-- End Single Item -->
+                </div>
+        </div>
+    </section>
+   
+
+  
+
+
+
+<!-- Service Page end -->
+
+    <!-- -------Footer----- -->
+    <?php
+include ('footer.php');
+?>
+
+    <!-- --------footer end---- -->
+
+    <!-- js -->
+    <script src="https://kit.fontawesome.com/f2786c0b81.js" crossorigin="anonymous"></script>
+</body>
+</html>    
